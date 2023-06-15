a136952f83efd101917464d9ac7cc23f22e0f91d
duong tran <kibzz0zzz@gmail.com>
2023-06-15 07:20:44 +0700

    DUONGTRAN: add logo app and merrging fishing screen

--- lib/screens/fishing_log/fishing_log_controller.dart
+++ lib/screens/fishing_log/fishing_log_controller.dart
@@ -15,6 +15,11 @@ class FishingLogController extends GetxController {
     try {
       final res = await FishingLogRepository.list;
       logs.assignAll(res);
+      if(logs.isNotEmpty && logs.length>=2 )
+        {
+          logs.sort((a,b)=> (b.releaseTime??DateTime.now()).compareTo(a.releaseTime??DateTime.now()));
+        }
+
       print(res.last.toJson());
     } catch (e) {}
   }
